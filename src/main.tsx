d0c2b0d6b22a53cceb6b82a95ba8fe165ebcf64d
rafaelvictor01 <rafaelvictor.bernardes@gmail.com>
2025-11-17 18:17:18 -0300

    [#3] - [SETUP] - Configuração do Biome no projeto

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './app.tsx'
 
+// biome-ignore lint/style/noNonNullAssertion: any
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
