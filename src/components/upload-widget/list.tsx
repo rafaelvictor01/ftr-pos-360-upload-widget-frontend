1d55560bd6eab0aad6da8944df6ccde89919e559
rafaelvictor01 <rafaelvictor.bernardes@gmail.com>
2025-11-17 21:16:01 -0300

    [#8] - [FEAT] - Inicio da criação da listagem para exibição dos arquivos enviados

--- src/components/upload-widget/list.tsx
+++ src/components/upload-widget/list.tsx
@@ -1,6 +1,18 @@
+import UploadWidgetListItem from "./list-tem"
+
 function UploadWidgetList() {
   return (
-    <div className="px-3 h-32">aa</div>
+    <div className="px-3 flex flex-col gap-3">
+      <span className="text-xs font-medium">
+        Uploaded files{' '}
+        <span className="text-zinc-400">(2)</span>
+      </span>
+
+      <div className="space-y-2">
+        <UploadWidgetListItem />
+        <UploadWidgetListItem />
+      </div>
+    </div>
   )
 }
 
